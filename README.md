900048757b0ef74ba54d45625832cb1626339d6a
olu@azimuthltd.net <olu@azimuthltd.net>
2018-01-29 00:08:14 +0000

    cleanup files, remove comments

--- README.md
+++ README.md
@@ -1,95 +1,40 @@
+# demo
+
+* [camperbnb.herokuapp.com](https://camperbnb.herokuapp.com/) - A demo version of this app has been deployed to Heroku
+
 # camper-bnb
 
-This project was completed as part of a Udemy course, it is a fully functional production version of the site. I made a whole host of changes to the original material.
+This project was completed as a fully functional production version camping booking website.
 
-# MyHacks
-
-```
-Added search by location on the home page
-
-Made a number of wholesale changes to the UI/UX - using a modified version of 
-the Gaia template from creative-Tim
-
-added a number of new routes
-added a get comment route
-
-Split out views into separate UI components
-
-Extended the user and campground models
-
-Added a number of routes for an authenticated user like My campgrounds, My Reviews
-
-Added ability for user to edit their own user profile and change passwords
-
-Added identicon support for users avatar using jdenticon.js
-
-Created an actual logo with a modified version of a logo from the Noun Project
-
-```
-# Installation
-
-- `git clone https://github.com/oluomoniyi/camper-bnb.git`
-- `cd [project folder]`
-- `npm install`
-
-# Running
-
-- `nodemon`
-
-# Additional Code Examples
-
-```
-//SEARCH POST ROUTE
-router.post("/search", function (req,res){ 
-      var search = req.body.search;
-	  if (search){
-		res.redirect("/search?q="+search)
-	  }
-	  else {
-	     res.redirect("/search")
-	  }
-})
-
-//SEARCH GET ROUTE
-router.get("/search", function (req,res){
-	var query = req.query["q"]
-	if (query){//if search then search
-        Campground.find({location: new RegExp(query, 'i')}, function(err, campgrounds){
-            if (err){
-                console.log(err)
-            }
-            else{
-                res.render("campgrounds/search", {campgrounds, query})
-            }
-        })
-    }
-    else {
-        Campground.find({}, function(err, campgrounds){
-            if (err){
-                console.log(err)
-            }
-            else{
-                res.render("campgrounds/search", {campgrounds, query: "/search"})
-            }
-        })
-    }
-})
-```
+The website was heavily inpsired by airbnb design I made a whole host of changes to the original material.
 
 # Motivation
 
 This project was a built as a side project over a few weeks, to pull all the knowledge I have gained over the years in front end dev into one project.
+
+# Installation
+
+* `git clone https://github.com/oluomoniyi/camper-bnb.git`
+* `cd camperbnb`
+* `npm install`
+
+# Running
+
+* You will need to run a mongo daemon with the mongod, or you can unc
+* `nodemon`
 
 # Useful Links
 
 * [OluOmoniyi](http://www.oluomoniyi.com) - personal website
 * [theazimuth.co](http://www.theazimuth.co) - Company Website
 
-* [udemy](https://www.udemy.com/the-web-developer-bootcamp/) - Udemy course, cant recommend it enough to be honest!
+* [udemy](https://www.udemy.com/the-web-developer-bootcamp/) - Udemy course, that could help you build something similar cant recommend it enough to be honest!
 * [unsplash](http://www.unsplash.com) - Amazing image source
 * [jdenticon](http://www.jdenticon.com) - Generates user avatar from random seed data
 * [nounproject](http://www.nounproject.com) - Starter user logo
 * [fontawesosome](http://www.fontawesosome.com) - font icons
+
+* [Inspired by Airbnb](https://www.airbnb.co.uk/)
 
 # License (MIT, Apache, etc.)
 
--- index.js
+++ index.js
@@ -1,85 +1,86 @@
 //INITIAL
-var express = require("express"), 
-        app = express(),
-        bodyParser = require("body-parser"),
-        request = require("request"),
-        mongoose = require("mongoose"),//.set('debug', true),debugging is extremely useful in development
-        flash = require("connect-flash"),
-        passport = require("passport"),
-        LocalStrategy = require("passport-local"),
-        Session = require("express-session"),
-        seedDB = require("./seeds"),
-        User = require("./models/user"),
-        UserComment = require("./models/comment"),
-        methodOverride = require("method-override"),
-        //ObjectId = require('mongoose').Types.ObjectId,
-        port = process.env.PORT || 3000,
-        url = process.env.DATABASEURL || "mongodb://localhost/mongo",
-        commentRoutes = require("./routes/comments"),
-        campgroundsRoutes = require("./routes/campgrounds"),
-        indexRoutes = require("./routes/index"),
-        userRoutes = require("./routes/user"),
-        api = require("./routes/api")
-  
-mongoose.connect(url)
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  request = require("request"),
+  mongoose = require("mongoose"), //.set('debug', true),debugging is extremely useful in development
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  Session = require("express-session"),
+  seedDB = require("./seeds"),
+  User = require("./models/user"),
+  UserComment = require("./models/comment"),
+  methodOverride = require("method-override"),
+  port = process.env.PORT || 3000,
+  url = process.env.DATABASEURL || "mongodb://localhost/mongo",
+  secret = process.env.PASSPORT_SECRET || "UULUQdm2ktLsKN",
+  commentRoutes = require("./routes/comments"),
+  campgroundsRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index"),
+  userRoutes = require("./routes/user"),
+  api = require("./routes/api");
+
+mongoose.connect(url);
 
 app.set("view engine", "ejs");
-app.use(bodyParser.json() );
+app.use(bodyParser.json());
 app.use(flash());
-app.use(bodyParser.urlencoded({extended:true}));//parse form data
-app.use(express.static(__dirname + "/public"))
-app.use(methodOverride("_method"))
+app.use(bodyParser.urlencoded({ extended: true })); //parse form data
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
 //seedDB();
 
 //passport config
-app.use(Session({
-    secret: 'UULUQdm2ktLsKNLhRYEz5Bff',
+app.use(
+  Session({
+    secret,
     resave: false,
-    saveUninitialized:false,
-}))
+    saveUninitialized: false
+  })
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 //passport configuration
 
-app.use(function(req,res,next){
-    res.locals.currentUser = req.user
-    res.locals.error = req.flash("error")
-    res.locals.success = req.flash("success")
-    next();
-})
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
 
-app.use(indexRoutes)
-app.use(campgroundsRoutes)
+app.use(indexRoutes);
+app.use(campgroundsRoutes);
 app.use(commentRoutes);
-app.use(userRoutes)
-app.use(api)
+app.use(userRoutes);
+app.use(api);
 
 //catch all 404 error page
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.status(404);
 
   // respond with html page
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
+  if (req.accepts("html")) {
+    res.render("404", { url: req.url });
     return;
   }
 
   // respond with json
-  if (req.accepts('json')) {
-    res.send({ error: 'Not found' });
+  if (req.accepts("json")) {
+    res.send({ error: "Not found" });
     return;
   }
 
   // default to plain-text. send()
-  res.type('txt').send('Not found');
+  res.type("txt").send("Not found");
 });
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log(process.env.PORT, process.env.IP, "running")
+app.listen(port, function() {
+  console.log("Example app listening on port 3000!");
 });
